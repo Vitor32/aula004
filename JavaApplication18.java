500790dcc802f4c0f8d1835a559dfd7d935b9f22
Vitor32 <42851863+Vitor32@users.noreply.github.com>
2018-08-30 20:54:44 -0300

    Add files via upload

--- JavaApplication18.java
+++ JavaApplication18.java
@@ -10,56 +10,59 @@ public class JavaApplication18 {
         
         System.out.println(mes); 
         
-                switch(); {
-        
-                System.out.println( " " ); 
+                switch(mes){        
+                
                         
                 case 1:  
                         
-                System.out.println();
-                        
+                System.out.println("janeiro");
+                break;        
                 case 2:  
                         
-                System.out.println();  
-                        
+                System.out.println("fevereiro");  
+                break;        
                 case 3: 
                         
-                System.out.println(); 
-                        
+                System.out.println("março"); 
+                break;        
                 case 4: 
                         
-                System.out.println();  
-                        
+                System.out.println("abril");  
+                break;        
                 case 5: 
                         
-                System.out.println(); 
-                        
+                System.out.println("maio"); 
+                break;        
                 case 6: 
                         
-                System.out.println();  
-                        
+                System.out.println("junho");  
+                break;        
                 case 7: 
                         
-                System.out.println();  
-                                                
+                System.out.println("julho");  
+                break;                                
                 case 8:  
                      
-                System.out.println();  
-                
+                System.out.println("agosto");  
+                break;
                 case 9:
                                                 
-                System.out.println();
-                 
+                System.out.println("setembro");
+                break; 
                 case 10: 
                         
-                System.out.println(); 
-                
+                System.out.println("outubro"); 
+                break;
                 case 11:   
                         
-                System.out.println(); 
-                
+                System.out.println("novembro"); 
+                break;
                 case 12: 
+                    
+                System.out.println("dezembro"); 
+                    
                 break;                    
                     }
     
+}
 }
