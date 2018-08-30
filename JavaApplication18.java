58cfa2716a820235b0e57246a5417a75231f4a9b
Vitor32 <42851863+Vitor32@users.noreply.github.com>
2018-08-30 20:52:00 -0300

    Add files via upload

---
+++ JavaApplication18.java
@@ -0,0 +1,65 @@
+
+package javaapplication18;
+
+
+public class JavaApplication18 {
+
+    
+    public static void main(String[] args) {
+        int mes = 12; 
+        
+        System.out.println(mes); 
+        
+                switch(); {
+        
+                System.out.println( " " ); 
+                        
+                case 1:  
+                        
+                System.out.println();
+                        
+                case 2:  
+                        
+                System.out.println();  
+                        
+                case 3: 
+                        
+                System.out.println(); 
+                        
+                case 4: 
+                        
+                System.out.println();  
+                        
+                case 5: 
+                        
+                System.out.println(); 
+                        
+                case 6: 
+                        
+                System.out.println();  
+                        
+                case 7: 
+                        
+                System.out.println();  
+                                                
+                case 8:  
+                     
+                System.out.println();  
+                
+                case 9:
+                                                
+                System.out.println();
+                 
+                case 10: 
+                        
+                System.out.println(); 
+                
+                case 11:   
+                        
+                System.out.println(); 
+                
+                case 12: 
+                break;                    
+                    }
+    
+}
